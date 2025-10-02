5135a1b087f63a838d3469b5ef4a959312968924
Aiden Fox Ivey <aiden@aidenfoxivey.com>
2025-10-02 13:05:41 -0400

    Opt to use Debug in lieu of impl Display

--- src/scanner.rs
+++ src/scanner.rs
@@ -252,7 +252,7 @@ pub struct Token {
     line: usize,
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum TokenType {
     LeftParen,
     RightParen,
@@ -294,50 +294,4 @@ enum TokenType {
     While,
 
     Eof,
-}
-
-impl Display for TokenType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TokenType::LeftParen => write!(f, "LeftParen"),
-            TokenType::RightParen => write!(f, "RightParen"),
-            TokenType::LeftBrace => write!(f, "LeftBrace"),
-            TokenType::RightBrace => write!(f, "RightBrace"),
-            TokenType::Comma => write!(f, "Comma"),
-            TokenType::Dot => write!(f, "Dot"),
-            TokenType::Minus => write!(f, "Minus"),
-            TokenType::Plus => write!(f, "Plus"),
-            TokenType::Semicolon => write!(f, "Semicolon"),
-            TokenType::Slash => write!(f, "Slash"),
-            TokenType::Star => write!(f, "Star"),
-            TokenType::Bang => write!(f, "Bang"),
-            TokenType::BangEqual => write!(f, "BangEqual"),
-            TokenType::Equal => write!(f, "Equal"),
-            TokenType::EqualEqual => write!(f, "EqualEqual"),
-            TokenType::Greater => write!(f, "Greater"),
-            TokenType::GreaterEqual => write!(f, "GreaterEqual"),
-            TokenType::Less => write!(f, "Less"),
-            TokenType::LessEqual => write!(f, "LessEqual"),
-            TokenType::Identifier => write!(f, "Identifier"),
-            TokenType::String => write!(f, "String"),
-            TokenType::Number => write!(f, "Number"),
-            TokenType::And => write!(f, "And"),
-            TokenType::Class => write!(f, "Class"),
-            TokenType::Else => write!(f, "Else"),
-            TokenType::False => write!(f, "False"),
-            TokenType::For => write!(f, "For"),
-            TokenType::Fun => write!(f, "Fun"),
-            TokenType::If => write!(f, "If"),
-            TokenType::Nil => write!(f, "Nil"),
-            TokenType::Or => write!(f, "Or"),
-            TokenType::Print => write!(f, "Print"),
-            TokenType::Return => write!(f, "Return"),
-            TokenType::Super => write!(f, "Super"),
-            TokenType::This => write!(f, "This"),
-            TokenType::True => write!(f, "True"),
-            TokenType::Var => write!(f, "Var"),
-            TokenType::While => write!(f, "While"),
-            TokenType::Eof => write!(f, "Eof"),
-        }
-    }
 }
